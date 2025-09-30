1c9e45e12e8b0967defae834ce7295b07439576b
igor4022 <tic80padraolua@gmail.com>
2025-09-30 02:48:48 -0300

    issoo

--- projetos.js
+++ projetos.js
@@ -15,8 +15,32 @@ elementos.forEach((elemen) => myObserver.observe(elemen))
 /*Texto dos projetos quando aperta o botão*/
 
 const calculadora = document.getElementById('calculadora')
+const login = document.getElementById('login')
+const jogo = document.getElementById('jogo')
+const cordel = document.getElementById('cordel')
+const relogio = document.getElementById('relogio')
 
 function texto1() {
 
-    calculadora.textContent = 'Uma calculadora digital que pode executar ecuações basícas'
+    calculadora.textContent = 'Uma calculadora digital que pode executar ecuações basícas.'
+}
+
+function texto2() {
+
+    login.textContent = 'Um login feito para indicar e autenticar um usuário em uma sistema digital.'
+}
+
+function texto3() {
+
+    jogo.textContent = 'Um jogo simples que é inspirado nas funções do jodo do dinossauro do Google.'
+}
+
+function texto4() {
+
+    cordel.textContent = 'Um pequeno site que fala sobre um poema e o divulga.'
+}
+
+function texto5() {
+
+    relogio.textContent = 'Um relogio de manual mas que na verda é digital.'
 }
