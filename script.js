c9bd90b50ca70e888db7a6492c6151719e54e86a
igor4022 <tic80padraolua@gmail.com>
2026-01-15 01:25:45 -0300

    blz

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const abaMenu = document.querySelectorAll('a')
 const somMenu = new Audio('sons/collect-points-190037 (1).mp3')
 const somClick = new Audio('sons/game-show-correct-tick-sound-416167.mp3')
 
-const fundo = document.querySelector('body')
+const fundo = document.querySelector('img')
 const somFundo = new Audio('sons/space-440026.mp3')
 
 function somAbaMenu() {
