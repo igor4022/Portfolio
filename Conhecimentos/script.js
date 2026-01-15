42aaac5554bfca37a07758a8325354f14f82936f
igor4022 <tic80padraolua@gmail.com>
2026-01-15 02:26:19 -0300

    ta

--- Conhecimentos/script.js
+++ Conhecimentos/script.js
@@ -8,47 +8,63 @@ const myObserver = new IntersectionObserver((verme) => {
     })
 })
 
+
 const elementos = document.querySelectorAll('.hidden')
 
 elementos.forEach((elemen) => myObserver.observe(elemen))
 
 /*Conteúdo de explicação das cartas*/
+function TextoExemplos() {
 
-const html = document.querySelector('.text-html')
-const css = document.querySelector('.text-css')
-const php = document.querySelector('.text-php')
-const js = document.querySelector('.text-js')
-const python = document.querySelector('.text-python')
-const java = document.querySelector('.text-java')
+    const html = document.querySelector('.text-html')
+    const css = document.querySelector('.text-css')
+    const php = document.querySelector('.text-php')
+    const js = document.querySelector('.text-js')
+    const python = document.querySelector('.text-python')
+    const java = document.querySelector('.text-java')
 
-function texto1() {
+    function texto1() {
 
-    html.textContent = ' É a linguagem padrão para criar a estrutura e o conteúdo de páginas web, usando tags para organizar texto, imagens, links e outros elementos. Ela define o que cada parte do conteúdo é (como um parágrafo ou um título), sendo o esqueleto básico da web. O HTML trabalha em conjunto com outras tecnologias, como o CSS para estilização e o JavaScript para interatividade, para criar sites completos. '
+        html.textContent = ' É a linguagem padrão para criar a estrutura e o conteúdo de páginas web, usando tags para organizar texto, imagens, links e outros elementos. Ela define o que cada parte do conteúdo é (como um parágrafo ou um título), sendo o esqueleto básico da web. O HTML trabalha em conjunto com outras tecnologias, como o CSS para estilização e o JavaScript para interatividade, para criar sites completos. '
 
+    }
+
+    function texto2() {
+
+        css.textContent = ' É uma linguagem de estilo que controla o visual e a apresentação de documentos escritos em linguagens de marcação, como o HTML. Ela separa o conteúdo da sua estrutura visual, permitindo que designers e desenvolvedores estilizem páginas web para torná-las mais atraentes e visualmente organizadas. Com o CSS, é possível definir cor, tamanho, fonte e espaçamento de elementos, além de controlar o layout, criar designs responsivos e adicionar animações e efeitos. '
+    }
+
+    function texto3() {
+
+        php.textContent = 'PHP é uma linguagem de programação de código aberto e server-side, ideal para criar sites e aplicações web dinâmicas. Ela pode gerar conteúdo personalizado, interagir com bancos de dados, processar dados de formulários, gerir cookies e autenticar utilizadores. As suas principais características incluem ser simples, flexível, robusta e compatível com praticamente todos os sistemas operativos. '
+
+    }
+
+    function texto4() {
+        
+        js.textContent = 'JavaScript é uma linguagem de programação de alto nível que torna as páginas da web interativas e dinâmicas, indo além de conteúdo estático. Ela permite a criação de animações, galerias de fotos, formulários interativos e atualizações em tempo real, tanto no navegador do utilizador (front-end) como no servidor (back-end). '
+    }
+
+    function texto5() {
+
+        python.textContent = 'Python é uma linguagem de programação de alto nível, orientada a objetos e fácil de aprender, conhecida pela sua sintaxe clara e legível. Ela pode ser usada para uma vasta gama de aplicações, incluindo desenvolvimento web, análise de dados, inteligência artificial, automação de tarefas e criação de aplicações desktop e jogos. A sua versatilidade e a grande quantidade de bibliotecas tornam-na popular para iniciantes e para projetos complexos. '
+    }
+
+    function texto6() {
+
+        java.textContent = 'Java é uma linguagem versátil para criar uma vasta gama de aplicações, desde sistemas empresariais robustos e back-ends web (incluindo microsserviços) até apps Android, softwares desktop, jogos e soluções de Big Data e IA, destacando-se pela portabilidade ("escreva uma vez, execute em qualquer lugar"), orientação a objetos e segurança, rodando em diversos sistemas operacionais graças à Máquina Virtual Java (JVM). '
+    }
 }
 
-function texto2() {
+TextoExemplos()
 
-    css.textContent = ' É uma linguagem de estilo que controla o visual e a apresentação de documentos escritos em linguagens de marcação, como o HTML. Ela separa o conteúdo da sua estrutura visual, permitindo que designers e desenvolvedores estilizem páginas web para torná-las mais atraentes e visualmente organizadas. Com o CSS, é possível definir cor, tamanho, fonte e espaçamento de elementos, além de controlar o layout, criar designs responsivos e adicionar animações e efeitos. '
+const botao = document.querySelectorAll('button')
+const somBtn = new Audio('Conhecimentos/sons/surprised-videogame-sfx-423628.mp3')
+
+function somClickBtn() {
+    somBtn.play()
 }
 
-function texto3() {
-
-    php.textContent = 'PHP é uma linguagem de programação de código aberto e server-side, ideal para criar sites e aplicações web dinâmicas. Ela pode gerar conteúdo personalizado, interagir com bancos de dados, processar dados de formulários, gerir cookies e autenticar utilizadores. As suas principais características incluem ser simples, flexível, robusta e compatível com praticamente todos os sistemas operativos. '
-
-}
-
-function texto4() {
-    
-    js.textContent = 'JavaScript é uma linguagem de programação de alto nível que torna as páginas da web interativas e dinâmicas, indo além de conteúdo estático. Ela permite a criação de animações, galerias de fotos, formulários interativos e atualizações em tempo real, tanto no navegador do utilizador (front-end) como no servidor (back-end). '
-}
-
-function texto5() {
-
-    python.textContent = 'Python é uma linguagem de programação de alto nível, orientada a objetos e fácil de aprender, conhecida pela sua sintaxe clara e legível. Ela pode ser usada para uma vasta gama de aplicações, incluindo desenvolvimento web, análise de dados, inteligência artificial, automação de tarefas e criação de aplicações desktop e jogos. A sua versatilidade e a grande quantidade de bibliotecas tornam-na popular para iniciantes e para projetos complexos. '
-}
-
-function texto6() {
-
-    java.textContent = 'Java é uma linguagem versátil para criar uma vasta gama de aplicações, desde sistemas empresariais robustos e back-ends web (incluindo microsserviços) até apps Android, softwares desktop, jogos e soluções de Big Data e IA, destacando-se pela portabilidade ("escreva uma vez, execute em qualquer lugar"), orientação a objetos e segurança, rodando em diversos sistemas operacionais graças à Máquina Virtual Java (JVM). '
-}
+botao.forEach((btn) => {
+    btn.addEventListener('click', somClickBtn)
+})
--- script.js
+++ script.js
@@ -1,8 +1,5 @@
 const abaMenu = document.querySelectorAll('a')
 const somMenu = new Audio('sons/collect-points-190037 (1).mp3')
-const somClick = new Audio('sons/game-show-correct-tick-sound-416167.mp3')
-
-const fundo = document.querySelector('img')
 const somFundo = new Audio('sons/space-440026.mp3')
 
 function somAbaMenu() {
@@ -10,19 +7,12 @@ function somAbaMenu() {
     somMenu.play()
 }
 
-function somAbaClick() {
-
-    somClick.play()
-}
-
-function somMenuFundo() {
-
+function somFundoMenu() {
     somFundo.play()
 }
 
 abaMenu.forEach((aba) => {
-    aba.addEventListener('mousemove', somAbaMenu)
-    aba.addEventListener('mousedown', somAbaClick)
+    aba.addEventListener('mouseenter', somAbaMenu)
 })
 
-fundo.addEventListener('mouseenter', somMenuFundo)
+window.addEventListener('mousemove', somFundoMenu)
