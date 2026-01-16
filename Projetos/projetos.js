3b2c0149f637a5dff471bdd550eb9ce81c64859b
igor4022 <tic80padraolua@gmail.com>
2026-01-16 01:23:40 -0300

    ta

--- Projetos/projetos.js
+++ Projetos/projetos.js
@@ -1,16 +1,21 @@
-const myObserver = new IntersectionObserver((verme) => {
-    verme.forEach((ver) => {
-        if(ver.isIntersecting){
-            ver.target.classList.add('show')
-        } else {
-            ver.target.classList.remove('show')
-        }
+function animacaoEntrada() {
+
+    const myObserver = new IntersectionObserver((verme) => {
+        verme.forEach((ver) => {
+            if(ver.isIntersecting){
+                ver.target.classList.add('show')
+            } else {
+                ver.target.classList.remove('show')
+            }
+        })
     })
-})
 
-const elementos = document.querySelectorAll('.hidden')
+    const elementos = document.querySelectorAll('.hidden')
 
-elementos.forEach((elemen) => myObserver.observe(elemen))
+    elementos.forEach((elemen) => myObserver.observe(elemen))
+}
+
+animacaoEntrada()
 
 /*Texto dos projetos quando aperta o botão*/
 
@@ -24,7 +29,6 @@ function texto1() {
 
     calculadora.textContent = 'Uma calculadora digital que pode executar ecuações basícas.'
 }
-
 function texto2() {
 
     login.textContent = 'Um login feito para indicar e autenticar um usuário em uma sistema digital.'
